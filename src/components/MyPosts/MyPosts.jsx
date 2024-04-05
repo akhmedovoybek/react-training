b6bcd7963c964a02a6fbc6fa3e7f8475fdcd0277
akhmedovoybek <akhmedovoybek2555@gmail.com>
2024-04-05 22:13:32 +0500

    React Samurai

--- src/components/MyPosts/MyPosts.jsx
+++ src/components/MyPosts/MyPosts.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import c from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
@@ -7,13 +8,16 @@ const MyPosts = (props) => {
   let postsElements = posts.map((p) => {
     return <Post message={p.message} likescount={p.likescount} />;
   });
-
+  let newPostElement = React.createRef();
+  let addPost = ()=>{
+    alert(newPostElement.current.value)
+  }
   return (
     <div>
       My posts
       <div>
-        <textarea class="resize-none border rounded-md p-4 w-full h-32 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"></textarea>
-        <button class="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 mb-3">
+        <textarea ref={newPostElement} className="resize-none border rounded-md p-4 w-full h-32 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"></textarea>
+        <button onClick={addPost} className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 mb-3">
           Add Post
         </button>
       </div>
